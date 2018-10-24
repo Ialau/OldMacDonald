2aa37798c7f9033c88653370ef7de59406a75146
ialau <ialau@s.sfusd.edu>
2018-10-24 08:08:34 -0700

    finished oldmacdonald

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,14 @@
-class NamedCow
+class NamedCow extends Cow
 {
-
+  private String myType;
+  private String mySound;
+  private String myName;
+ public NamedCow(String type, String sound, String name){
+   myType = type;
+   mySound = sound;
+   myName = name;
+ }
+ public String getSound(){return mySound;}
+ public String getType(){return myType;}
+ public String getName(){return myName;}
 }
